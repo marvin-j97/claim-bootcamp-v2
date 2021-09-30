106edb5377f5c0cb6dd9bbde3ba5956e178b2d5c
marvin-j97 <33938500+marvin-j97@users.noreply.github.com>
2021-09-30 14:25:25 +0200

    (fix) typo

--- src/8.ts
+++ src/8.ts
@@ -2,7 +2,7 @@ const EPSILON = 0.00000001;
 const h = EPSILON;
 
 /**
- * Returns the derivate of a function
+ * Returns the derivative of a function
  * https://en.wikipedia.org/wiki/Derivative
  * http://www.malinc.se/math/calculus/diffatpointen.php
  * https://sites.google.com/a/case.edu/differentiation/_/rsrc/1364892409693/difference-quotient/dq.png
