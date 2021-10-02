bca69a36473b548909cb9e36ef7be2a21a8d4bf9
marvin-j97 <33938500+marvin-j97@users.noreply.github.com>
2021-10-02 15:52:42 +0200

    add comments to 10

--- src/10.ts
+++ src/10.ts
@@ -1,17 +1,30 @@
 export type Tokenizer = (str: string) => Array<string>;
 
 export class InvertedIndex {
+  /**
+   * Inverted index data
+   * Each token points to a set of document IDs
+   */
   private index: Record<string, Set<number>> = {};
 
+  /**
+   * Add document IDs for token to inverted index
+   */
   indexTokens(id: number, tokens: string[]): void {
     // TODO: implement
   }
 
+  /**
+   * Get the document IDs referenced by the token
+   */
   getDocumentIdsByToken(token: string): number[] {
     // TODO: implement
     return [];
   }
 
+  /**
+   * Remove the given document ID from the index
+   */
   removeDocument(id: number): void {
     // TODO: implement
   }
@@ -23,6 +36,7 @@ export interface ISearchHit {
 }
 
 export class SearchEngine {
+  // Tokenizer converts a text body into tokens
   private tokenizer: Tokenizer;
   // TODO: add more properties as needed
 
@@ -30,14 +44,30 @@ export class SearchEngine {
     this.tokenizer = tokenizer;
   }
 
+  /**
+   * Add document to index
+   * 1. Create tokens from text body
+   * 2. Add tokens to inverted index
+   */
   indexDocument(id: number, text: string): void {
     // TODO: implement
   }
 
+  /**
+   * Removes a document from index
+   */
   removeDocument(id: number): void {
     // TODO: implement
   }
 
+  /**
+   * Searches documents using search query and ranks them by relevance
+   * 1. Tokenize query
+   * 2. For each token, find relevant documents
+   * 3. Keep track of how often documents are referenced
+   * 4. Sort search hits by score and return as list
+   *    First item should have highest relevance
+   */
   retrieveDocuments(query: string): Array<ISearchHit> {
     // TODO: implement
     return [];
